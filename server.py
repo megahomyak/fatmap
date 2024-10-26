793079a5fc586e551582e6d276bee29347fd69ad
megahomyak <g.megahomyak@gmail.com>
2024-10-26 16:22:23 +0500

    Trying to write this in Python, decided to rewrite with JS Bun

--- server.py
+++ server.py
@@ -1,110 +1,25 @@
-STATUS_CODE_OK = 200
-STATUS_CODE_UNAUTHORIZED = 500
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import sqlite3
 
-class Server:
-    def send_response(self, status_code: int, response: "json Python object"):
-        self.send_response(status_code)
+class FatMapServer(BaseHTTPRequestHandler):
+    def get_layer(self):
+        pass
+
+    def get_value(self):
+        print(self.rfile.read())
+
+    def add_layer(self):
+        pass
+
+    def do_GET(self):
+        self.send_response(200)
         self.end_headers()
-        self.wfile.write(json_str.encode())
-
-#####################
-#####################
-#####################
-#####################
-#####################
-
-
-
-
-
-
-
-
-
-
-UnsignedInt = int
-Value = UnsignedInt
-DEFAULT_VALUE = 0
-Key = UnsignedInt
-
-
-class Layer:
-    def __init__(self, key: Key, value: Value):
-        self.key = key
-        self.value = value
-
-
-class LayerId:
-    def __init__(self, value: UnsignedInt):
-        self.value = value
-
-    def next(self):
-        self.value += 1
-
-
-class NotFound: pass
-
-
-class HistoryInterface:
-    def add_layer(self, key: Key, value: Value) -> None:
-        raise NotImplementedError()
-
-    def get_last_value_or_default(self, key: Key) -> Value:
-        raise NotImplementedError()
-
-    def get_layer(self, id: LayerId) -> Layer | NotFound:
-        raise NotImplementedError()
-
-
-class FatMap:
-    def __init__(self, history: HistoryInterface):
-        self.history = history
-
-    def add_layer(self, layer: Layer) -> "old Value":
-        old_value = self.get_value(layer.key)
-        history.add_layer(layer)
-        return old_value
-
-    def get_value(self, key: Key) -> Value:
-        return self.history.get_last_value_or_default(key)
-
-    def get_layer(self, id: LayerId) -> Layer | NotFound:
-        return self.history.get_layer(id)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        {
+            "/get_layer": self.get_layer,
+            "/get_value": self.get_value,
+            "/add_layer": self.add_layer,
+        }[self.path]()
+        self.wfile.write(b'Hello, World!')
+
+httpd = HTTPServer(('', 8000), FatMapServer)
+httpd.serve_forever()
